be70e4f097519b2aa9c16d7ebd9863ac2b7714ea
Ross Breytberg <rossbreytberg@gmail.com>
2013-04-06 21:38:50 -0700

    added colorpicker

--- app/assets/javascripts/polls.js
+++ app/assets/javascripts/polls.js
@@ -3,6 +3,26 @@
 
 var poll_option_count = 2;
 
+function on_color_select(hex) {}
+
+function show_colorpicker(event) {
+  on_color_select = function(hex) {
+    var color_input = $(event.target);
+    color_input.css('color', hex).val(hex);
+  };
+  var colorpicker = $('#colorpicker');
+  var x_offset = colorpicker.outerWidth() / 2;
+  var y_offset = colorpicker.outerHeight() / 2;
+  $('#colorpicker').css({'left': event.pageX - x_offset, 'top': event.pageY - y_offset }).show();
+  $(document).click(hide_colorpicker);
+  return false;
+}
+
+function hide_colorpicker() {
+  $(document).unbind('click', hide_colorpicker);
+  $('#colorpicker').hide();
+}
+
 function add_poll_option() {
   var poll_options = $('.poll_option');
   var new_poll_option = poll_options.first().clone().addClass('removable_poll_option');
@@ -42,6 +62,11 @@ function on_filter_change() {
 }
 
 $(document).ready(function() {
+  ColorPicker(document.getElementById('colorpicker'), function(hex) {
+    on_color_select(hex);
+  });
+  $('#colorpicker').click(function(e) {e.stopPropagation();});
+  $('.color_input').click(show_colorpicker);
   $('.link_to_add_poll_option').click(add_poll_option);
   $('.link_to_remove_poll_option').click(remove_poll_option);
   $('#filter').bind('change', on_filter_change);
